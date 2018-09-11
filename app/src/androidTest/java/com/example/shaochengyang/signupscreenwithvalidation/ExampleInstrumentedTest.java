121b1e4f6ce28c1c9d0fcaf7e1251eecf99200b4
ysc1995 <39675591+ysc1995@users.noreply.github.com>
2018-09-10 22:41:56 -0400

    test

--- app/src/androidTest/java/com/example/shaochengyang/signupscreenwithvalidation/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/shaochengyang/signupscreenwithvalidation/ExampleInstrumentedTest.java
@@ -7,6 +7,12 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +28,12 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.shaochengyang.signupscreenwithvalidation", appContext.getPackageName());
+    }
+
+    @Test
+    public void textInputOutput() {
+        onView(withId(R.id.userNameText)).perform(typeText("andrew"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.userNameText)).check(matches(withText("andrew")));
     }
 }
